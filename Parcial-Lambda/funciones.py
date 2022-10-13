39f235d24b1944ccbd9e132e15e57db14786e9e0
Thiago Vilani <thiagovilani19@gmail.com>
2022-10-13 15:22:57 -0300

    Añado clase 14

--- Parcial-Lambda/funciones.py
+++ Parcial-Lambda/funciones.py
@@ -7,7 +7,7 @@ import json
 #5 - Exportar lista personajes a CSV
 #6 - Salir
 
-ruta = r"C:\Users\vilan\Desktop\Programación & Laboratorio I\Programacion-y-Laboratorio-1\Parcial-Lambda\data.json"
+ruta = r"C:\Users\vilan\OneDrive\Escritorio\Programación & Laboratorio I\Programacion-y-Laboratorio-1\Parcial-Lambda\data.json"
 
 def cargar_json(ruta:str):
     """
@@ -22,33 +22,34 @@ def cargar_json(ruta:str):
 lista_personajes = cargar_json(ruta).copy()
 
 #:::::::::::::::::::ORDENAR LA LISTA POR ALTURA:::::::::::::::::::::
-def pasar_int(personaje):
-    personaje["height"] = int(personaje["height"])
-    return personaje
-lista_personajes_copia = list(map(pasar_int, lista_personajes))
-def pasar_altura(personaje):
-    return personaje["height"]
-lista_personajes_copia.sort(key = pasar_altura)
+def ordenar_lista_altura(lista:list):
+    def pasar_int(personaje):
+        personaje["height"] = int(personaje["height"])
+        return personaje
+    lista_personajes_copia = list(map(pasar_int, lista))
+    def pasar_altura(personaje):
+        return personaje["height"]
+    lista_personajes_copia.sort(key = pasar_altura)
+    return lista_personajes_copia
 #XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-#:::::::::::::
+
+#:::::::::::::::::PERSONAJES MAS ALTOS SEGUN GENERO::::::::::::::::::
 lista_masculinos = list(filter(lambda personaje : personaje["gender"] == "male", lista_personajes))
 lista_femeninos = list(filter(lambda personaje : personaje["gender"] == "female", lista_personajes))
 lista_na = list(filter(lambda personaje : personaje["gender"] == "n/a", lista_personajes))
 
-def pasar_altura_masculino(personaje):
-    return personaje["height"]
-    
-def pasar_altura_femenino(personaje):
+def pasar_altura(personaje):
     return personaje["height"]
 
-def pasar_na(personaje):
-    return personaje["height"]
+mensaje_altos = "El hombre mas alto es {0}\nLa mujer mas alta es {1}\nEl nada mas alto es {2}".format(
+    (max(lista_masculinos, key = pasar_altura)["name"]), 
+    (max(lista_femeninos, key = pasar_altura)["name"]), 
+    (max(lista_na, key = pasar_altura)["name"]))
+print(mensaje_altos) 
+#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-lista_masculinos.sort(key = pasar_altura_masculino)
-lista_femeninos.sort(key = pasar_altura_femenino)
-lista_na.sort(key = pasar_na)
-print("El hombre mas alto es {}".format(lista_masculinos[-1]))
-print("La mujer mas alta es {}".format(lista_femeninos[-1]))
-print("El nada mas alto es {}".format(lista_na[-1]))
 
+#:::::::::::::::::::::::::ORDENAR POR PESO::::::::::::::::::::::::::
+
+#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
